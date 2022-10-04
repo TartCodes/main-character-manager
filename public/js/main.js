ea2fe14b9493a1daa2e972d0ba912c71125875a1
TartCodes <tartcodes@gmail.com>
2022-10-04 14:51:15 -0400

    working on mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,15 @@
 
-//weapon info
-// const melee = document.getElementById('addMelee')
-// const meleeDiv = document.getElementById('melee-weapon')
 //show shield info button
 const shieldButton = document.getElementById('shield-button')
-let showHideShield = document.getElementById('shield-info')
 //text swap for shield button
+const showHideShield = document.getElementById('shield-info')
+//keeping hidden open on refresh
+// const shieldInfo = localStorage.getItem()
 
-
-
-
-// Adding/Removing Weapons
-
-//the modal window
-    // const meleeModal = document.getElementById('melee-modal')
-//button to open the modal window
-    // const showWeapon = document.getElementById('addMelee')
-//close the modal
-    // const closeMeleeBtn = document.getElementById('close-melee')
-//add the weapon to the dom
-// const showWeapon = document.getElementById('add-weapon')
-//access melee content
-    // const meleeContent = document.getElementById('melee-content')
-//div to append to?
-    // const appended = document.getElementById('weapon-buttons')
-
-// addMeleeBtn.addEventListener('click', openMelee);
-    // closeMeleeBtn.addEventListener('click', closeMelee);
-// showWeapon.addEventListener('click', createWeapon)
-    // window.addEventListener('click', outsideClick);
 
 //button for showing and hiding shield information
 shieldButton.onclick = function() {    
-    if(showHideShield.style.display === 'flex'){ 
+        if(showHideShield.style.display === 'flex'){ 
         showHideShield.style.display = 'none'
     }else{
         showHideShield.style.display = 'flex'
@@ -49,28 +26,6 @@ shieldButton.addEventListener('click', function shieldButtonTextSwap() {
 })
 
 
-//opens the melee modal window
-// function openMelee(){    
-//     meleeModal.style.display = 'flex'
-// }
-
-//closes the melee modal window
-// function closeMelee(){    
-//     meleeModal.style.display = 'none'
-// }
-
-//close the modal(s) clicking outside window
-// function outsideClick(e) {
-//     if (e.target == meleeModal) {
-//       meleeModal.style.display = 'none';
-//     }
-//   }
-
-// function createWeapon() {
-//     const temp = document.getElementsByTagName("template")[0];
-//     const clon = temp.content.cloneNode(true);
-//     document.body.appendChild(clon);
-// }
 
 
 
