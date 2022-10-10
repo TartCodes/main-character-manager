b5a2cd8858ec80f89c9992919a0aebe97935a50e
TartCodes <tartcodes@gmail.com>
2022-10-10 15:27:14 -0400

    all levels show on click = bad

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,32 @@
 const shieldButton = document.getElementById('shield-button')
 //text swap for shield button
 const showHideShield = document.getElementById('shield-info')
-//keeping hidden open on refresh
-// const shieldInfo = localStorage.getItem()
+//level buttons
+// const levelButtons = document.getElementsByClassName('level-button')
+const levelTextArea = document.querySelectorAll('[id^=levelText]')
 
+
+
+//button for levels
+for(let showText of document.getElementsByClassName('level-button')){
+    showText.onclick = function(){     
+        for(let i = 0; i < levelTextArea.length; i++){
+            if(levelTextArea[i].style.display === 'none'){
+                levelTextArea[i].style.display = 'initial'
+            } else {
+                levelTextArea[i].style.display = 'initial'
+            }
+        }
+        
+
+        // if(levelTextArea.style.display === 'none'){
+        //     levelTextArea.style.display = 'none'
+        // } else {
+        //     levelTextArea.style.display = 'initial'
+        //     }
+        // }
+    }
+}
 
 //button for showing and hiding shield information
 shieldButton.onclick = function() {    
