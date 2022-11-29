9eab8b12669a1cc40a08004485b6ad205813fb53
TartCodes <tartcodes@gmail.com>
2022-11-29 14:16:33 -0500

    fixed levels buttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,46 +1,22 @@
-//level buttons
-// const levelButtons = document.getElementsByClassName('level-button')
-// const levelTextArea = document.querySelectorAll("[id^=levelText]");
-
-// //button for levels
-// for (let showText of document.getElementsByClassName("level-button")) {
-//   showText.onclick = function () {
-//     for (let i = 0; i < levelTextArea.length; i++) {
-//       if (levelTextArea[i].style.display === "none") {
-//         levelTextArea[i].style.display = "initial";
-//       } else {
-//         levelTextArea[i].style.display = "initial";
-//       }
-//     }
-
-// if(levelTextArea.style.display === 'none'){
-//     levelTextArea.style.display = 'none'
-// } else {
-//     levelTextArea.style.display = 'initial'
-//     }
-// }
-//   };
-// }
-
 class DisplayInfo {
   //shield button text
   shieldButtonText = document.getElementById("shield-button");
   //shield info display
   showHideShield = document.getElementById("shield-info");
-  //level button
-  //   levelButton = document.querySelectorAll('[id^="lvl"]'); //<-- fix
-  //   levelsTextarea = document.querySelectorAll('[id^="level"]');
+  //get textareas
+  levelsTextarea = document.getElementsByClassName("level-text");
+  //get parent node which is a div
+  levelsDiv = document.getElementsByClassName("level-div");
 
-  //NEED TO MAKE THIS SMALLER - REFACTOR
-  //lvl one
-  levelOneBtn = document.getElementById("one-button");
-  levelOneTa = document.getElementById("levelText1");
-  //lvl two
-  levelTwoBtn = document.getElementById("two-button");
-  levelTwoTa = document.getElementById("levelText2");
-  //level three
-  levelThreeBtn = document.getElementById("three-button");
-  levelThreeTa = document.getElementById("levelText3");
+  handleClick() {
+    for (let i = 0; i < this.levelsDiv.length; i++) {
+      this.levelsDiv[i].addEventListener("click", () => {
+        this.levelsTextarea[i].style.display === ""
+          ? (this.levelsTextarea[i].style.display = "initial")
+          : (this.levelsTextarea[i].style.display = "");
+      });
+    }
+  }
 
   displayShieldInfo() {
     this.shieldButtonText.onclick = () => {
@@ -54,36 +30,10 @@ class DisplayInfo {
       } else {
         this.shieldButtonText.innerText = "Show Shield";
       }
-      console.log(this.levelButton[0]);
     };
-  }
-
-  //figure out how to loop through and only display 1
-  levelDisplay() {
-    this.levelOneBtn.addEventListener("click", (e) => {
-      this.levelOneTa.style.display === ""
-        ? (this.levelOneTa.style.display = "initial")
-        : (this.levelOneTa.style.display = "");
-    });
-    this.levelTwoBtn.addEventListener("click", (e) => {
-      this.levelTwoTa.style.display === ""
-        ? (this.levelTwoTa.style.display = "initial")
-        : (this.levelTwoTa.style.display = "");
-    });
-    this.levelThreeBtn.addEventListener("click", (e) => {
-      this.levelThreeTa.style.display === ""
-        ? (this.levelThreeTa.style.display = "initial")
-        : (this.levelThreeTa.style.display = "");
-    });
   }
 }
 
-// .onclick = () => {
-//     for (let i = 0; i < this.levelsTextarea.length; i++) {
-//       console.log(this.levelsTextarea.levelTwo);
-//     }
-//   };
-
 let showInputs = new DisplayInfo();
 showInputs.displayShieldInfo();
-showInputs.levelDisplay();
+showInputs.handleClick();
